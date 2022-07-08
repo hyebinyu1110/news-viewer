06553a56d838775d753e27473c2af3727444c783
hyebin yu <hyebinyu1110@gmail.com>
2022-07-08 16:57:47 +0900

    code until 14.5

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React, { useState } from 'react';
 import axios from "axios";
+import NewsList from './components/NewsList';
 
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=8c18e150c391487a94e2dba3b0d657eb',
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
+
   return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} />}
-    </div>
+    <NewsList />
   )
 }
 
