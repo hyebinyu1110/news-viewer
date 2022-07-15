69f2f70a5c264a1d7b8160d3baf99d946b7266f5
hyebinyu <hyebinyu1110@gmail.com>
2022-07-16 00:05:58 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import { useState, useCallback} from 'react';
-import {Routes, Route} from 'react-router-dom';
-import NewsPage from './pages/NewsPage';
-
+import React, {useState} from 'react';
+import axios from 'axios';
 
 const App = () => {
-  const [category, setCategory] = useState('all');
-  const onSelect = useCallback(category =>setCategory(category), []);
+  const [data, setData] = useState(null);
+  const onClick = async () =>{
+    try{
+    const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+      setData(response.data);
+    }catch(e){
+      console.log(e);
+    }
+  };
 
   return (
-    <Routes>
-      <Route path="/" element={<NewsPage />}/>
-      <Route path="/:category" element={<NewsPage />} />
-    </Routes>
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {
+        data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readonly={true} />
+      }
+    </div>
   );
 };
 
