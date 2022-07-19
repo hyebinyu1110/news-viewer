1827f31cc13be0c1f41123bd5c2b274b33c69728
hyebin yu <hyebinyu1110@gmail.com>
2022-07-19 16:34:22 +0900

    repractice from beginning

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React, { useState } from 'react';
-import axios from "axios";
 import NewsList from './components/NewsList';
-
 
 const App = () => {
 
   return (
-    <NewsList />
+    <div>
+      <NewsList />
+    </div>
   )
-}
 
+}
 export default App;
